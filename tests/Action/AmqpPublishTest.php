9becf8f029ea2143f1dca752506c865d68401b2f
Christoph Kappestein <k42b3.x@gmail.com>
2016-10-05 14:45:36 +0200

    wait for message

--- tests/Action/AmqpPublishTest.php
+++ tests/Action/AmqpPublishTest.php
@@ -68,6 +68,12 @@ class AmqpPublishTest extends AmqpTestCase
         $channel->queue_declare('foo_queue', false, true, false, false);
         $channel->basic_qos(null, 1, null);
         $channel->basic_consume('foo_queue', '', false, false, false, false, [$callback, 'callback']);
+
+        while (count($channel->callbacks)) {
+            $channel->wait();
+        }
+
+        $channel->close();
     }
 
     public function testGetForm()
