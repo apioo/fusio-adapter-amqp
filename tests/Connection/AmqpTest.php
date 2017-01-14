008d0b0e7850c143fa18eff337bb476e7187d503
Christoph Kappestein <k42b3.x@gmail.com>
2017-01-14 11:13:11 +0100

    add connection test

---
+++ tests/Connection/AmqpTest.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Adapter\Amqp\Tests\Connection;
+
+use Fusio\Adapter\Amqp\Connection\Amqp;
+use Fusio\Engine\Form\Builder;
+use Fusio\Engine\Form\Container;
+use Fusio\Engine\Form\Element\Input;
+use Fusio\Engine\Parameters;
+use Fusio\Engine\Test\EngineTestCaseTrait;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+/**
+ * AmqpTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class AmqpTest extends \PHPUnit_Framework_TestCase
+{
+    use EngineTestCaseTrait;
+
+    public function testGetConnection()
+    {
+        /** @var Amqp $connection */
+        $connection = $this->getConnectionFactory()->factory(Amqp::class);
+
+        $config = new Parameters([
+            'host'     => '127.0.0.1',
+            'port'     => 5672,
+            'user'     => 'guest',
+            'password' => 'guest',
+            'vhost'    => '/'
+        ]);
+
+        $client = $connection->getConnection($config);
+
+        $this->assertInstanceOf(AMQPStreamConnection::class, $client);
+    }
+
+    public function testConfigure()
+    {
+        $connection = $this->getConnectionFactory()->factory(Amqp::class);
+        $builder    = new Builder();
+        $factory    = $this->getFormElementFactory();
+
+        $connection->configure($builder, $factory);
+
+        $this->assertInstanceOf(Container::class, $builder->getForm());
+
+        $elements = $builder->getForm()->getProperty('element');
+        $this->assertEquals(5, count($elements));
+        $this->assertInstanceOf(Input::class, $elements[0]);
+        $this->assertInstanceOf(Input::class, $elements[1]);
+        $this->assertInstanceOf(Input::class, $elements[2]);
+        $this->assertInstanceOf(Input::class, $elements[3]);
+        $this->assertInstanceOf(Input::class, $elements[4]);
+    }
+}
