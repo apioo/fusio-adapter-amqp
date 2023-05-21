2766e9833ac007cecb81c1972677511416bbdf55
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-21 10:38:15 +0200

    extend connection abstract

--- src/Connection/Amqp.php
+++ src/Connection/Amqp.php
@@ -22,7 +22,7 @@
 namespace Fusio\Adapter\Amqp\Connection;
 
 use Fusio\Engine\Connection\PingableInterface;
-use Fusio\Engine\ConnectionInterface;
+use Fusio\Engine\ConnectionAbstract;
 use Fusio\Engine\Form\BuilderInterface;
 use Fusio\Engine\Form\ElementFactoryInterface;
 use Fusio\Engine\ParametersInterface;
@@ -35,7 +35,7 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
  * @license http://www.gnu.org/licenses/agpl-3.0
  * @link    https://www.fusio-project.org/
  */
-class Amqp implements ConnectionInterface, PingableInterface
+class Amqp extends ConnectionAbstract implements PingableInterface
 {
     public function getName(): string
     {
